1dc34daaf542556e30094b614622ed3c35e7100c
abraham4show <shorinwaafolabi@gmail.com>
2026-07-16 01:19:32 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,9 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  server: {
+    preset: 'node-server', // Change this if it is set to cloudflare
+  },
 });
+
+
